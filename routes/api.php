7923b4e7762b7d3405bbc7f2c0b0325e7ea64a3f
yikikdeveloper <ahsentekdemir.sh@gmail.com>
2021-07-11 16:20:14 +0300

    feat: sqlitedb, product get, post endpoints

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/products', function(){
+    return Product::all();
+});
+
+Route::post('/products', function(){
+    return Product::create([
+        'name' => 'Product Name',
+        'slug' => 'product-name',
+        'description' => 'Product Description',
+        'price' => '21.21'
+    ]);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
