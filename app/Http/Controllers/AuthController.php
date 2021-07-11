2a47b51503b0c3d1b26f72f83f520f39084c2b08
yikikdeveloper <somethingsmeels@gmail.com>
2021-07-11 17:47:04 +0300

    feat: login controls and generate token for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,4 +31,36 @@ class AuthController extends Controller
         return response($response, 201);
 
     }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email'=> 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'error with creds'
+            ], 401);
+        }
+        
+        $token = $user->createToken('token')->planTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'logged out'
+        ];
+    }
 }
