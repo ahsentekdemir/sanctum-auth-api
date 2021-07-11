fa7940bfab0386fe3b7297ec22f16a13cfcacdd4
yikikdeveloper <somethingsmeels@gmail.com>
2021-07-11 16:59:00 +0300

    feat: update, delete, search functions

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -76,7 +76,9 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
     }
 
     /**
@@ -87,6 +89,6 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return Product::destroy($id);
     }
 }
